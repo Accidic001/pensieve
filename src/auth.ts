e088063af089506fe8d6ada135ecc76e3c86fd34
Muhammad Abubakar Jamiu <muhammadabubakarjamiu@gmail.com>
2025-04-30 23:02:35 +0100

    fix nextauth

--- src/auth.ts
+++ src/auth.ts
@@ -1,32 +1,29 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import {PrismaAdapter} from "@auth/prisma-adapter"
-import { db } from "@/db"
-import { AdapterUser } from "next-auth/adapters"
-import { Session } from "next-auth"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
 
-
-
-const GITHUB_CLIENT_ID=process.env.GITHUB_CLIENT_ID
-const GITHUB_CLIENT_SECRET=process.env.GITHUB_CLIENT_SECRET
-
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  throw new Error("missing github client id or secret")
-}
-
- 
-export const { handlers:{GET,POST}, auth, signIn, signOut } = NextAuth({
-    adapter: PrismaAdapter(db),
-  providers: [GitHub({
-    clientId: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
-  })],
+export const { 
+  handlers: { GET, POST }, 
+  auth, 
+  signIn, 
+  signOut 
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // Add this line
   callbacks: {
-    async session({ session, user }: { session: Session; user: AdapterUser }) {
-      if (session.user && user) {
-        session.user.id = user.id
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
       }
-      return session
-    }}
-})
-
+      return session;
+    }
+  },
+  trustHost: true, // Recommended for Vercel deployments
+});
